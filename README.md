2574a6784003fd58dba68b2788fa53992f4d8d70
deepak <deepak@regelcloud.com>
2021-03-21 14:24:45 +0530

    fix typos and add  example usage

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # terraform-google-network-peering
 Terraform module for google network peering
+
+Modules usage:
+
+```
+module "network-peering" {
+  source  = "regelcloud/network-peering/google"
+  version = "1.0.2"
+  network1_id = var.network1_id
+  network2_id = var.network2_id
+  name = var.name
+}
+
+```
--- output.tf
+++ output.tf
@@ -1,24 +1,24 @@
-output "network1_ppering_id" {
+output "network1_peering_id" {
   value = "${google_compute_network_peering.peering1.id}"
 }
 
-output "network2_ppering_id" {
+output "network2_peering_id" {
   value = "${google_compute_network_peering.peering2.id}"
 }
 
-output "network1_ppering_state" {
+output "network1_peering_state" {
   value = "${google_compute_network_peering.peering1.state}"
 }
 
-output "network2_ppering_state" {
+output "network2_peering_state" {
   value = "${google_compute_network_peering.peering2.state}"
 }
 
-output "network1_ppering_state_details" {
+output "network1_peering_state_details" {
   value = "${google_compute_network_peering.peering1.state_details}"
 }
 
-output "network2_ppering_state_details" {
+output "network2_peering_state_details" {
   value = "${google_compute_network_peering.peering2.state_details}"
 }
 
